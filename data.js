39b4a112fcd47a2f495e5c1751cc36eb001860e7
de-salman <salmanfarisi.bca@gmail.com>
2023-09-28 12:53:32 +0530

    menu dynamic

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const categoryData = [
   {
-    Starters: [
+    category: "Starters",
+    items: [
       {
         name: "Falafel",
         price: "18",
@@ -16,19 +17,14 @@ const categoryData = [
       {
         name: "Risotto shrimps",
         price: "53 dirham",
-        description: "Risotto shrimps with red cary paste",
+        description: "Risotto shrimps with red curry paste",
         image: "/catMenu/081A1753.jpg",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        image: "",
       },
     ],
   },
   {
-    Salads: [
+    category: "Salads",
+    items: [
       {
         name: "Artichoke salad",
         price: "39",
@@ -38,21 +34,223 @@ const categoryData = [
       {
         name: "Fattoush salad",
         price: "33",
-        description: "mixed vegtables with fried bread",
+        description: "mixed vegetables with fried bread",
         image: "/catMenu/081A2983.jpg",
       },
       {
         name: "Tabbouleh salad",
         price: "32",
-        description: "tradional taboleh",
+        description: "traditional tabbouleh",
         image: "/catMenu/081A2963.jpg",
       },
       {
         name: "Caesar Salad",
         price: "39",
-        description: "lettce caesar souse with chicken",
+        description: "lettuce caesar sauce with chicken",
         image: "/catMenu/081A2983.jpg",
       },
     ],
   },
+  {
+    category: "Shawarma",
+    items: [
+      {
+        name: "Bowl of Shawarma",
+        price: "39",
+        description: "",
+        image: "",
+      },
+      {
+        name: "Pita shawarma",
+        price: "28",
+        description: "",
+        image: "",
+      },
+      {
+        name: "Fried peta shwarma",
+        price: "32",
+        description: "",
+        image: "",
+      },
+    ],
+  },
+  {
+    category: "Main course",
+    items: [
+      {
+        name: "Chicken filets",
+        price: "56",
+        description: "served with grilled vegetables",
+        image: "",
+      },
+      {
+        name: "Beef filet cubes",
+        price: "82",
+        description: "served with mashed celery root, chimichurri,",
+        image: "",
+      },
+      {
+        name: "Beef burger",
+        price: "59",
+        description: "green rocca, tomato, onion, pickles, aioli mustard",
+        image: "",
+      },
+      {
+        name: "Turkish kebab",
+        price: "48",
+        description: "served with matbookha, grilled vegetables,tahini",
+        image: "",
+      },
+      {
+        name: "Mixed Grilled",
+        price: "225",
+        description: "Mix grill shared 8 skewers, grilled vegetables",
+        image: "",
+      },
+      {
+        name: "Cast Iron Meatballs",
+        price: "56",
+        description: "Mediterranean meatballs and yogurt . Served with rice.",
+        image: "",
+      },
+      {
+        name: "Full Roasted Chicken",
+        price: "62",
+        description: "Served with ground vegetables and rice",
+        image: "",
+      },
+      {
+        name: "Moroccan Tagging",
+        price: "45",
+        description: "Couscous with chicken and red sauce",
+        image: "",
+      },
+      {
+        name: "Chicken mix parts",
+        price: "59",
+        description: "liver, chicken breast, chicken hearts",
+        image: "",
+      },
+      {
+        name: "Chicken scallop",
+        price: "57",
+        description: "served with spicy sauce, one side to choose from",
+        image: "",
+      },
+      {
+        name: "Premium steak",
+        price: "182",
+        description: "ask waiter",
+        image: "",
+      },
+    ],
+  },
+  {
+    category: "From the Sea",
+    items: [
+      {
+        name: "Salmon in the oven",
+        price: "92",
+        description: "Served with mushrooms risotto",
+        image: "",
+      },
+      {
+        name: "Fried Mediterranean sea bream",
+        price: "126",
+        description: "served with risotto green freica",
+        image: "",
+      },
+      {
+        name: "Mix Seafood",
+        price: "225",
+        description:
+          "fried squid tubes, king shrimps, salmon fish, big mulle, butter lemon sauce",
+        image: "",
+      },
+    ],
+  },
+  {
+    category: "Pizza",
+    items: [
+      {
+        name: "Margarita pizza",
+        price: "45",
+        description: "tomato sauce mozzarella cheese",
+        image: "",
+      },
+      {
+        name: "Roma pizza",
+        price: "64",
+        description: "Served with cherry tomatoes and artichoke",
+        image: "",
+      },
+      {
+        name: "New york pizza",
+        price: "62",
+        description: "Served with pepperoni and red onion",
+        image: "",
+      },
+    ],
+  },
+  {
+    category: "GEORGIA",
+    items: [
+      {
+        name: "Khachapuri Ajarski",
+        price: "45",
+        description: "",
+        image: "",
+      },
+      {
+        name: "Khachapuri Megruli",
+        price: "35",
+        description: "",
+        image: "",
+      },
+    ],
+  },
+  {
+    category: "Penne pasta",
+    items: [
+      {
+        name: "Penne pasta",
+        price: "58",
+        description:
+          "A base of white or red sauce. Extra fishmeat 10 AED - Extra vegetables 5 AED",
+        image: "",
+      },
+    ],
+  },
+  {
+    category: "Our Desserts",
+    items: [
+      {
+        name: "Kenaffah",
+        price: "38",
+        description:
+          "A base of white or red sauce. Extra fishmeat 10 AED - Extra vegetables 5 AED",
+        image: "",
+      },
+      {
+        name: "Choclate lava cake",
+        price: "42",
+        description: "",
+        image: "",
+      },
+      {
+        name: "Apple crumble",
+        price: "37",
+        description: "",
+        image: "",
+      },
+      {
+        name: "beriut nights",
+        price: "32",
+        description: "",
+        image: "",
+      },
+    ],
+  },
 ];
+
+export default categoryData;
--- pages/menu.js
+++ pages/menu.js
@@ -1,4 +1,4 @@
-import { useState, React} from "react";
+import { useState, React, useEffect } from "react";
 import MenuBanner from "@/Components/Home-components/MenuBanner";
 import HomeReviews from "@/Components/Home-components/Home-Reviews/HomeReviews";
 import InstagramComponent from "@/Components/Home-components/Instagram/Instagram";
@@ -7,8 +7,13 @@ import Head from "next/head";
 import Slider from "react-slick";
 import useWindowDimensions from "@/hooks/useWindowDimensions";
 import Link from "next/link";
+import categoryData from "../data";
 const about = () => {
   const [open, setOpen] = useState(false);
+  const [CD, setCD] = useState(categoryData);
+  const [activeItem, setActiveItem] = useState(0);
+  const [activeDish, setActiveDish] = useState(0);
+
   const settings = {
     dots: true,
     infinite: false,
@@ -47,191 +52,103 @@ const about = () => {
           <div className="MenuHeader2">Sulme Menu</div>
           <div className="MenuRowListWrap menuSlider">
             <Slider {...settings}>
-              <div className="MenuItem active">
-                <div className="MenuItemGrid m1">
-                  <div className="MenuName">Starters</div>
+              {CD.map((items, index) => (
+                <div
+                  className={`MenuItem ${
+                    activeItem === index ? "active" : null
+                  }`}
+                  onClick={() => setActiveItem(index)}
+                >
+                  <div
+                    className="MenuItemGrid"
+                    style={{ backgroundImage: `url(${items.items[0].image})` }}
+                  >
+                    <div className="MenuName">{items.category}</div>
+                  </div>
                 </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m2">
-                  <div className="MenuName">Salads</div>
-                </div>
-              </div>
-              <div className="MenuItem ">
-                <div className="MenuItemGrid m3">
-                  <div className="MenuName">Shawarma</div>
-                </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m4">
-                  <div className="MenuName">Main Course</div>
-                </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m5">
-                  <div className="MenuName">From The Sea</div>
-                </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m6">
-                  <div className="MenuName">Pizza</div>
-                </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m7">
-                  <div className="MenuName">Georgia</div>
-                </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m8">
-                  <div className="MenuName">Penne Pasta</div>
-                </div>
-              </div>
-              <div className="MenuItem">
-                <div className="MenuItemGrid m9">
-                  <div className="MenuName">Desserts</div>
-                </div>
-              </div>
+              ))}
             </Slider>
           </div>
           <div className="MenuItemLiasWrap">
             <div className="row">
-              <div className="col-md-6"  onClick={() => setOpen(true)}>
-                <div className="PrItem">
-                  <div className="PrItemLeft">
-                    <div className="PrItemThumb">
-                      <Image
-                        src="/menuimages/thumb/starter3.jpg"
-                        layout="responsive"
-                        width={"300"}
-                        height={"300"}
-                        priority={true}
-                        className=""
-                      />
+              {CD[activeItem].items.map((dish, index) => (
+                <div
+                  className="col-md-6"
+                  onClick={() => {
+                    setOpen(true), setActiveDish(index);
+                  }}
+                >
+                  <div className="PrItem">
+                    <div className="PrItemLeft">
+                      <div className="PrItemThumb" style={{ height: "100%" }}>
+                        <img
+                          src={dish.image}
+                          width={"110"}
+                          height={"110%"}
+                          style={{ height: "inherit", objectFit: "cover" }}
+                        />
+                      </div>
+                    </div>
+                    <div className="PrItemMiddle">
+                      <div className="PrItemName">{dish.name}</div>
+                      <div className="PrItemDesc">{dish.description}</div>
+                      <div className="PrItemPrice">{dish.price}</div>
+                    </div>
+                    <div className="PrItemRight">
+                      <div className="ReadMrBtn">Read More</div>
                     </div>
                   </div>
-                  <div className="PrItemMiddle">
-                    <div className="PrItemName">hummus</div>
-                    <div className="PrItemDesc">Tradional hummus</div>
-                    <div className="PrItemPrice">AED 25</div>
-                  </div>
-                  <div className="PrItemRight">
-                    <div className="ReadMrBtn">Read More</div>
-                  </div>
                 </div>
-              </div>
-              <div className="col-md-6" onClick={() => setOpen(true)}>
-                <div className="PrItem">
-                  <div className="PrItemLeft">
-                    <div className="PrItemThumb">
-                      <Image
-                        src="/menuimages/thumb/salads.jpg"
-                        layout="responsive"
-                        width={"300"}
-                        height={"300"}
-                        priority={true}
-                        className=""
-                      />
-                    </div>
-                  </div>
-                  <div className="PrItemMiddle">
-                    <div className="PrItemName">Falafel</div>
-                    <div className="PrItemDesc">Tradional hummus</div>
-                    <div className="PrItemPrice">AED 25</div>
-                  </div>
-                  <div className="PrItemRight">
-                    <div className="ReadMrBtn">Read More</div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6" onClick={() => setOpen(true)}>
-                <div className="PrItem">
-                  <div className="PrItemLeft">
-                    <div className="PrItemThumb">
-                      <Image
-                        src="/menuimages/thumb/shawarma.jpg"
-                        layout="responsive"
-                        width={"300"}
-                        height={"300"}
-                        priority={true}
-                        className=""
-                      />
-                    </div>
-                  </div>
-                  <div className="PrItemMiddle">
-                    <div className="PrItemName">Risotto shrimps</div>
-                    <div className="PrItemDesc">Tradional hummus</div>
-                    <div className="PrItemPrice">AED 25</div>
-                  </div>
-                  <div className="PrItemRight">
-                    <div className="ReadMrBtn">Read More</div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6" onClick={() => setOpen(true)}>
-                <div className="PrItem">
-                  <div className="PrItemLeft">
-                    <div className="PrItemThumb">
-                      <Image
-                        src="/menuimages/thumb/main.jpg"
-                        layout="responsive"
-                        width={"300"}
-                        height={"300"}
-                        priority={true}
-                        className=""
-                      />
-                    </div>
-                  </div>
-                  <div className="PrItemMiddle">
-                    <div className="PrItemName">Fried calamari</div>
-                    <div className="PrItemDesc">Tradional hummus</div>
-                    <div className="PrItemPrice">AED 25</div>
-                  </div>
-                  <div className="PrItemRight">
-                    <div className="ReadMrBtn">Read More</div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
           {open && (
-          <div className="MenuPopupWrap">
-            <div className="MenuPopup">
-              <div className="full-width">
-              <div className="MenuPopupImg position-relative">
-                <div className="CloseModal"  onClick={() => setOpen(false)}>&nbsp;</div>
-                <Image
-                  src="/menuimages/original/a.jpg"
-                  layout="responsive"
-                  width={"1000"}
-                  height={"665"}
-                  className=""
-                />
-              </div>
-              <div className="MenuPopupContent">
-                <div className="MenuPopupT1">Risoto Shrimps</div>
-                <div className="MenuPopupT2">Traditional Hummus</div>
-                <div className="MenuPopupT3">AED 54</div>
-              </div>
-              <div className="MenuPopupFooter">
-                <div className="BannerBtns">
-                  <a
-                    href="https://www.talabat.com/uae/sul-me-restaurant-llc"
-                    target="_blank"
-                    className="BannerBtn1"
-                  >
-                    Order Now
-                  </a>
+            <div className="MenuPopupWrap">
+              <div className="MenuPopup">
+                <div className="full-width">
+                  <div className="MenuPopupImg position-relative">
+                    <div className="CloseModal" onClick={() => setOpen(false)}>
+                      &nbsp;
+                    </div>
+                    <Image
+                      src="/menuimages/original/a.jpg"
+                      layout="responsive"
+                      width={"1000"}
+                      height={"665"}
+                      className=""
+                    />
+                  </div>
+                  <div className="MenuPopupContent">
+                    <div className="MenuPopupT1">
+                      {categoryData[activeItem].items[activeDish].name}
+                    </div>
+                    <div className="MenuPopupT2">
+                      {categoryData[activeItem].items[activeDish].description}
+                    </div>
+                    <div className="MenuPopupT3">
+                      AED&nbsp;
+                      {categoryData[activeItem].items[activeDish].price}
+                    </div>
+                  </div>
+                  <div className="MenuPopupFooter">
+                    <div className="BannerBtns">
+                      <a
+                        href="https://www.talabat.com/uae/sul-me-restaurant-llc"
+                        target="_blank"
+                        className="BannerBtn1"
+                      >
+                        Order Now
+                      </a>
 
-                  <Link href="/reservation" className="BannerBtn2">
-                    Reserve Now
-                  </Link>
+                      <Link href="/reservation" className="BannerBtn2">
+                        Reserve Now
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
-              </div>
             </div>
-          </div>
-           )}
+          )}
         </div>
       </div>
       <InstagramComponent />
